7a8de72f918954691757c57eff980ef48463fb80
TemryL <tommery01@gmail.com>
2022-11-24 00:13:24 +0100

    add jquery + all graphs

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-$('#user').change(function () {
-    var user = $('#user').val();
-    var graph = `graphs/length_sequences_graph_${user}`;
-    console.log(graph);
-    $('#len_graph').attr('data', graph);
+$(document).ready(function () {
+    $('#user').change(function () {
+        var user = $('#user').val();
+        var graph_count = `graphs/sequences_count/sequences_count_${user}.html`;
+        var graph_len = `graphs/sequences_length/sequences_length_${user}.html`;
+        var graph_len_merged = `graphs/merged_sequences_length/merged_sequences_length_${user}.html`;
+        $('#sequences_count').attr('data', graph_count);
+        $('#sequences_length').attr('data', graph_len);
+        $('#merged_sequences_length').attr('data', graph_len_merged);
+    });
 });
