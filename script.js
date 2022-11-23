c55180c91036d661d39c15e3bb4ede418e1b6a81
TemryL <tommery01@gmail.com>
2022-11-23 22:28:04 +0100

    pour loic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $('#user').change(function () {
     var user = $('#user').val();
     var graph = `graphs/length_sequences_graph_${user}`;
+    console.log(graph);
     $('#len_graph').attr('data', graph);
 });
